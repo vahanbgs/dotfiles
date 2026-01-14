eff2915dae70a693d893292ba6d85fe13a25d943
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-01-14 20:25:42 +0100

    fix: set $SHELL to fish in config.fish instead of .env

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set SHELL fish
     set fish_greeting
     direnv hook fish | source
     starship init fish | source
