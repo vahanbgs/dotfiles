d69f98adb55b614e421fbbf883c72e31b0d49073
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-01-15 18:47:06 +0100

    feat: add abbreviations for ls and ll in config.fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set SHELL fish
     set fish_greeting
+
+    abbr -a ls lsd -1
+    abbr -a ll lsd -l
+
     direnv hook fish | source
     starship init fish | source
 end
