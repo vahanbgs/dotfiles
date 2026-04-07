5859a2776226ae46ce5acfaab295ae82b8269746
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-04-07 19:28:43 +0200

    feat: use Rosé Pine Moon for fish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set SHELL fish
     set fish_greeting
+    fish_config theme choose "Rosé Pine Moon"
 
     abbr -a ls lsd -1
     abbr -a ll lsd -l
