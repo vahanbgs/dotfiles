a97d3dbfe7407848ce110cfa21f0e10312a1a25e
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-04-07 19:52:56 +0200

    feat: add age and sops to home.nix

--- home/.config/home-manager/home.nix
+++ home/.config/home-manager/home.nix
@@ -14,6 +14,7 @@
   home.packages =
     with pkgs;
     [
+      age
       anyrun
       bat
       biome
@@ -36,6 +37,7 @@
       nixfmt
       ripgrep
       sd
+      sops
       sqruff
       starship
       strace
