3b0a60d123f921e95d04abb75b27f193ba2d3c1a
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-04-09 16:00:25 +0200

    feat: add jj abbreviations for git subcommands

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,6 +8,19 @@ if status is-interactive
     abbr -a ll lsd -l
     abbr -a tree lsd --tree
 
+    function jj_sub
+        for subcommand in clone fetch init push
+            if string match --quiet 'jj '$subcommand' ' -- (commandline -j)
+                echo git $subcommand
+                return 0
+            end
+        end
+
+        return 1
+    end
+
+    abbr -a jj_sub --position anywhere --regex '([a-z]+)' --function jj_sub
+
     direnv hook fish | source
     starship init fish | source
 end
