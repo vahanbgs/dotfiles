e20ecac85b66819bcab5e40163ce072f18794c8e
vahanbgs <51419074+vahanbgs@users.noreply.github.com>
2026-06-25 23:49:06 +0200

    fix: replace jj abbreviations with functions because abbreviations were not working reliably

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -8,18 +8,27 @@ if status is-interactive
     abbr -a ll lsd -l
     abbr -a tree eza --tree --git-ignore
 
-    function jj_sub
-        for subcommand in clone fetch init push remote
-            if string match --quiet 'jj '$subcommand' ' -- (commandline -j)
-                echo git $subcommand
-                return 0
-            end
+    # function jj_sub
+    #     for subcommand in clone fetch init push remote
+    #         if string match --quiet 'jj '$subcommand' ' -- (commandline -j)
+    #             echo git $subcommand
+    #             return 0
+    #         end
+    #     end
+
+    #     return 1
+    # end
+
+    # abbr -a jj_sub --position anywhere --regex '([a-z]+)' --function jj_sub
+
+    function jj
+        switch $argv[1]
+            case clone fetch init push remote
+                jj git $argv
+            case '*'
+                command jj $argv
         end
-
-        return 1
     end
-
-    abbr -a jj_sub --position anywhere --regex '([a-z]+)' --function jj_sub
 
     direnv hook fish | source
     starship init fish | source
